70ebf37ea554c332d5ca4a2406c6a0e4e60105f9
RogueException <rogueexception@gmail.com>
2018-07-29 16:05:52 -0700

    Fixed several ResizableMemoryStream issues

--- src/Voltaic.Core/ResizableMemoryStream.cs
+++ src/Voltaic.Core/ResizableMemoryStream.cs
@@ -4,15 +4,17 @@ using System.IO;
 
 namespace Voltaic
 {
+    // TODO: Test
     public class ResizableMemoryStream : Stream
     {
         private ResizableMemory<byte> _buffer;
         private int _position;
-
+        
         public override bool CanRead => true;
         public override bool CanSeek => true;
         public override bool CanWrite => true;
 
+        public ref ResizableMemory<byte> Buffer => ref _buffer;
         public override long Length => _buffer.Length;
         public override long Position
         {
@@ -26,29 +28,31 @@ namespace Voltaic
         }
 
         public ResizableMemoryStream(int initialCapacity, ArrayPool<byte> pool = null)
-            : this(new ResizableMemory<byte>(initialCapacity, pool)) { }
-        public ResizableMemoryStream(ResizableMemory<byte> buffer)
         {
-            _buffer = buffer;
+            _buffer = new ResizableMemory<byte>(initialCapacity, pool);
         }
 
         public override int Read(byte[] buffer, int offset, int count)
         {
             int remaining = _buffer.Length - _position;
-            if (count < remaining)
+            if (remaining < count)
                 count = remaining;
-            _buffer.AsReadOnlySpan(offset, count).CopyTo(buffer.AsSpan(offset, count));
+            _buffer.AsReadOnlySpan(_position, count).CopyTo(buffer.AsSpan(offset, count));
+            Position += count;
             return count;
         }
         public override void Write(byte[] buffer, int offset, int count)
         {
-            buffer.AsSpan(offset, count).CopyTo(_buffer.AsSpan(_position, count));
+            var span = _buffer.RequestSpan(_position, count);
+            buffer.AsSpan(offset, count).CopyTo(span);
+            _buffer.Advance(count);
+            Position += count;
         }
 
         public override void SetLength(long value)
         {
             if (value > int.MaxValue)
-                throw new InvalidOperationException("ResizableMemoryStream does not support collections larger than Int32.MaxValue elements");
+                throw new InvalidOperationException($"{nameof(ResizableMemoryStream)} does not support streams larger than Int32.MaxValue elements");
             _buffer.Length = (int)value;
         }
         public override long Seek(long offset, SeekOrigin origin)
